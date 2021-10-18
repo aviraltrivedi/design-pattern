24df974477d51856239cedaaa920af8bff999321
pranavpande <pranavpande19>
2021-10-18 22:56:38 +0530

    Factory pattern implementation

--- src/factory/pattern/MainFactoryPattern.java
+++ src/factory/pattern/MainFactoryPattern.java
@@ -1,11 +1,15 @@
 package factory.pattern;
 
+import factory.pattern.patternService.FactoryPattern;
+
 import java.io.IOException;
 
 public class MainFactoryPattern {
 
-    public static void main (String[] args) throws IOException
+    public static void main (String[] args)
     {
         System.out.println("Hello");
+        FactoryPattern fp = new FactoryPattern();
+        fp.personType.apply("Teacher");
     }
 }
---
+++ src/factory/pattern/model/Person.java
@@ -0,0 +1,4 @@
+package factory.pattern.model;
+
+public class Person {
+}
