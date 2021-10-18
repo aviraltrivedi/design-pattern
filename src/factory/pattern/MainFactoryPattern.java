f84d9f0e853ef682c621f4fa806fb307dd250543
pranavpande <pranavpande19>
2021-10-18 22:01:35 +0530

    First commit

---
+++ src/factory/pattern/MainFactoryPattern.java
@@ -0,0 +1,11 @@
+package factory.pattern;
+
+import java.io.IOException;
+
+public class MainFactoryPattern {
+
+    public static void main (String[] args) throws IOException
+    {
+        System.out.println("Hello");
+    }
+}
